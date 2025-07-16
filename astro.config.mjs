a20b48a8645b98e1560d80c9f26ffb1dd9a55bad
zhuima <lang-zilangzi@163.com>
2025-07-16 21:41:07 +0800

    remove vercel.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import vercel from '@astrojs/vercel/serverless'
 export default defineConfig({
   output: 'server',
   adapter: vercel({
-    nodeVersion: '22'
+    nodeVersion: '18'
   }),
   integrations: [
     UnoCSS({
