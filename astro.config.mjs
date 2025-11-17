0afd2b7ffc0121e75c701c32a0898a1a14b6cb02
zhuima <zhuima314@gmail.com>
2025-11-17 16:02:16 +0800

    fix api search

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config'
 import UnoCSS from 'unocss/astro'
-import node from '@astrojs/node'
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid', // 混合模式：静态页面 + 服务端页面
-  adapter: node({ mode: 'standalone' }), // 使用Node.js适配器支持混合模式
+  adapter: vercel(), // 使用Vercel适配器支持混合模式
   integrations: [
     UnoCSS({
       injectReset: true,
